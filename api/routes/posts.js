bc5e2bfb863eed70668d696b5304960d55c393c3
Sentrydeca <63970490+Sentrydeca@users.noreply.github.com>
2023-06-16 23:37:34 +0530

    Further updates

---
+++ api/routes/posts.js
@@ -0,0 +1,69 @@
+const router = require("express").Router();
+const User = require("../models/User");
+const Post = require("../models/Post");
+
+
+// CREATE POST
+router.put("/:id", async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (post.username === req.body.username){
+
+            try {
+                const updatedPost = await Post.findByIdAndUpdate(req.params.id, {
+                    $set: req.body,
+                }, 
+                { new: true });
+                res.status(200).json(updatedPost);
+            } catch (err) {
+                res.status(500).json(err);
+            }
+        } else {
+            res.status(401).json("You can update your post only.");
+        }
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// DELETE POST
+router.delete("/:id", async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (post.username === req.body.username){
+
+            try {
+                await post.delete();
+                res.status(200).json("Post has been deleted...");
+            } catch (err) {
+                res.status(500).json(err);
+            }
+        } else {
+            res.status(401).json("You can delete your post only.");
+        }
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// GET POST
+router.get("/:id", async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        res.status(200).json(others);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// ALL POSTS
+router.get("/", async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        res.status(200).json(others);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;

873a7ae20188a85b04ec4a3f7c241eb2b0a87435
Sentrydeca <63970490+Sentrydeca@users.noreply.github.com>
2023-06-17 08:47:41 +0530

    Created categories and posts API

--- api/index.js
+++ api/index.js
@@ -5,6 +5,8 @@ const mongoose = require("mongoose");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
+const categoryRoute = require("./routes/categories");
+const multer = require("multer");
 
 dotenv.config();
 app.use(express.json());
@@ -15,13 +17,25 @@ mongoose.connect(process.env.MONGO_URL, {
     // useCreateIndex: true,
 }).then(console.log("Connected to mongoDB")).catch((err) => console.log(err));
 
-// app.use("/", (req, res) => {
-//   console.log("This is blog-website")
-// });
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "images");
+    },
+
+    filename: (req, file, cb) => {
+        cb(null, req.body.name)
+    },
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+    res.status(200).json("File has been uploaded");
+});
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
+app.use("/api/categories", categoryRoute);
 
 app.listen("5000", () => {
     console.log("Backend is running.");
--- api/routes/posts.js
+++ api/routes/posts.js
@@ -1,9 +1,18 @@
 const router = require("express").Router();
-const User = require("../models/User");
 const Post = require("../models/Post");
 
-
 // CREATE POST
+router.post("/", async (req, res) => {
+    const newPost = new Post(req.body);
+    try {
+        const savedPost = newPost.save();
+        res.status(200).json(savedPost);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+// UPDATE POST
 router.put("/:id", async (req, res) => {
     try {
         const post = await Post.findById(req.params.id);
@@ -58,12 +67,23 @@ router.get("/:id", async (req, res) => {
 
 // ALL POSTS
 router.get("/", async (req, res) => {
+    const username = req.query.user;
+    const catName = req.query.cat;
     try {
-        const post = await Post.findById(req.params.id);
-        res.status(200).json(others);
+        let posts;
+        if(username){
+            posts = await Post.find({ username });
+        } else if (catName) {
+            posts = await Post.find({ categories: {
+                $in: [catName]
+            }});
+        } else {
+            posts = await Post.find();
+        }
+         res.status(200).json(posts);
     } catch (err) {
         res.status(500).json(err);
-    }
+        }
 });
 
 module.exports = router;
